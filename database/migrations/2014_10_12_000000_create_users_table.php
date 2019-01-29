51cfde366d8b6fae33f7c45b2c526a64fcf29410
darkraul79 <info@raulsebastian.es>
2019-01-29 21:40:40 +0100

    Campos Usuario add

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname')->nullable();
+            $table->string('phone',15)->nullable();
+            $table->string('address')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('LOPD')->defatul(true);
+            $table->boolean('active')->defatul(true);
             $table->rememberToken();
             $table->timestamps();
         });
