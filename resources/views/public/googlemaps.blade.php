0913ea5e48649cacc52ebfae55992b4cbc186d1a
Jorge Alférez <usuario@pro-de-usuario.home>
2019-05-16 11:22:32 +0200

    Revisión de responsive y añadido Google Maps Javascript API

--- resources/views/public/googlemaps.blade.php
+++ resources/views/public/googlemaps.blade.php
@@ -1,1 +1,1 @@
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwpuM_K-Oo5QC8GMkeLHtcbVROQvUxntk&callback=initMap&region=ES" async defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwpuM_K-Oo5QC8GMkeLHtcbVROQvUxntk&callback=initMap&region=ES" type="text/javascript" async defer></script>
--- resources/views/public/puntosatencion.blade.php
+++ resources/views/public/puntosatencion.blade.php
@@ -24,162 +24,212 @@
 @endsection
   
 @section('scripts')
+
 <script>
-	function initMap() {
-
+	
+    function initMap() {
+        var madrid = {lat: 40.4346123, lng: -3.6788531};
+        var lasrozas = {lat: 39.4346123, lng: -3.6788531};
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            disableDefaultUI: true
-        });
-
-        var geocoder = new google.maps.Geocoder;
-        geocoder.geocode({
-            'address': 'Calle Diego de León, 50 Madrid'
-        }, function (results, status) {
-            if (status === 'OK') {
-                map.setCenter(results[0].geometry.location);
-                map.setOptions({
-                    styles: styles['silver']
-                });
-                new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location,
-                    icon: 'images/ico-maps.png',
-                });
-            } else {
-                window.alert('Geocode was not successful for the following reason: ' +
-                    status);
-            }
-        });
-        var styles = {
-            default: null,
-            silver: [{
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#f5f5f5'
-                    }]
+          zoom: 13,
+          center: madrid,
+          styles: [
+                {
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#f5f5f5"
+                    }
+                  ]
                 },
                 {
-                    elementType: 'labels.icon',
-                    stylers: [{
-                        visibility: 'off'
-                    }]
+                  "elementType": "labels.icon",
+                  "stylers": [
+                    {
+                      "visibility": "off"
+                    }
+                  ]
                 },
                 {
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#616161'
-                    }]
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#616161"
+                    }
+                  ]
                 },
                 {
-                    elementType: 'labels.text.stroke',
-                    stylers: [{
-                        color: '#f5f5f5'
-                    }]
+                  "elementType": "labels.text.stroke",
+                  "stylers": [
+                    {
+                      "color": "#f5f5f5"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'administrative.land_parcel',
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#bdbdbd'
-                    }]
+                  "featureType": "administrative.land_parcel",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#bdbdbd"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'poi',
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#eeeeee'
-                    }]
+                  "featureType": "poi",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#eeeeee"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'poi',
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#757575'
-                    }]
+                  "featureType": "poi",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#757575"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'poi.park',
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#e5e5e5'
-                    }]
+                  "featureType": "poi.park",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#e5e5e5"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'poi.park',
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#9e9e9e'
-                    }]
+                  "featureType": "poi.park",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#9e9e9e"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'road',
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#ffffff'
-                    }]
+                  "featureType": "road",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#ffffff"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'road.arterial',
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#757575'
-                    }]
+                  "featureType": "road.arterial",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#757575"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'road.highway',
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#dadada'
-                    }]
+                  "featureType": "road.highway",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#dadada"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'road.highway',
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#616161'
-                    }]
+                  "featureType": "road.highway",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#616161"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'road.local',
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#9e9e9e'
-                    }]
+                  "featureType": "road.local",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#9e9e9e"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'transit.line',
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#e5e5e5'
-                    }]
+                  "featureType": "transit.line",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#e5e5e5"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'transit.station',
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#eeeeee'
-                    }]
+                  "featureType": "transit.station",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#eeeeee"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'water',
-                    elementType: 'geometry',
-                    stylers: [{
-                        color: '#c9c9c9'
-                    }]
+                  "featureType": "water",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "color": "#c9c9c9"
+                    }
+                  ]
                 },
                 {
-                    featureType: 'water',
-                    elementType: 'labels.text.fill',
-                    stylers: [{
-                        color: '#9e9e9e'
-                    }]
+                  "featureType": "water",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                      "color": "#9e9e9e"
+                    }
+                  ]
                 }
-            ],
-        }
-    }
+              ]
+        });
+        
+        
+        <!-- SEDE CEPYME --/>
+        
+        
+        var cepyme = new google.maps.Marker({
+            position: madrid,
+            map: map,
+            icon: 'images/ico-maps.png',
+            title: 'CEPYME'
+        });
+        
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h2 id="cepyme" class="firstHeading">CEPYME</h2>'+
+            '<div id="bodyContent">'+
+            '<p><b>Dirección:</b> Calle de Diego de León, 50, 28006 Madrid<br/><b>Teléfono:</b>914 11 61 61</p>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        
+        cepyme.addListener('click', function() {
+          infowindow.open(map, cepyme);
+        });
+        <!-- SEDE CEPYME --/>
+        
+      }
 
 </script>
-@endsection
 
 @include('public.googlemaps')
+
+@endsection
+
+
